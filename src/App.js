b03bed6d7e0d8fb7eb66dee251a533e2aa5da445
Arunava Sarkar <sarkararunava00@gmail.com>
2024-01-11 13:02:16 +0530

    new push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header"
+import Main from "./components/Main"
+import { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [city, setCity] = useState("kolkata");
+  const [data, setData] = useState(null);
+  const handleOnchange = (event) => {
+    // console.log(event);
+    setCity(event.target.value);
+  }
+
+  const fetchApi = async () => {
+    const key = "ff602702c12304067c38dbd0d6ee3eda";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+    const response = await fetch(url);
+    const resJSON = await response.json();
+    //console.log(resJSON);
+    setData(resJSON);
+  }
+
+  const handleOnclick = () => {
+    //console.log("cliicked");
+
+    fetchApi();
+
+  }
+
+  useEffect(() => {
+    handleOnclick();
+  }, []);
+  //console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {data !=null ? <Header handleOnchange={handleOnchange} city={city} handleOnclick={handleOnclick} data={data}/> : null}
+     
+      {data != null ? <Main data={data} /> : null}
     </div>
   );
 }
